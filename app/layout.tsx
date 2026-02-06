e04b6df783dc40967fcbe523792efdb4a9e51300
AKILI P V S S SRI SWAROOP KUMAR <joyboysskofficially@gmail.com>
2026-02-06 17:02:32 +0530

    added site data and logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Kickstart | Project Guidance & Technical Mentoring",
-  description: "Structured support for ECE and CSE students in final-year and personal technical projects. Get expert guidance for project ideation, development, and execution.",
+  title: "Kickstart | Project Guidance & Technical Support",
+  description: "Structured support for students and tech enthusiasts for personal or professional full-scale technical projects. Get expert guidance for project ideation, development, and execution...",
 };
 
 export default function RootLayout({
@@ -20,6 +20,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+<head>
+
+        {/* <link rel="icon" href="./favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/android-chrome-512x512.png" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta property="og:image" content="https://www.kickstartnetwork.in/community.png" />
+        <meta property="og:image:alt" content="Kickstart your Project with us!" />
+        <meta name="twitter:image" content="https://www.hairdonations555.org/images/poster.jpg" />
+        <meta property="og:title" content="Kickstart Networks" />
+        <meta property="og:description" content="Structured support for students and tech enthusiasts for personal or professional full-scale technical projects. Get expert guidance for project ideation, development, and execution...." />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image:alt" content="Kickstart Networks" />
+      </head>
+
       <body className={`${poppins.variable} font-sans antialiased`}>
         {children}
       </body>
